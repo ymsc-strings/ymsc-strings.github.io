2d6a5696a8e78958b313df89aff7f019608d0dee
Wenbin <wenbin.yan@gmail.com>
2019-08-06 15:31:22 -0400

    Create handler.php

---
+++ html-email-form-with-attachment/handler.php
@@ -0,0 +1,27 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/*
+Tested working with PHP5.4 and above (including PHP 7 )
+
+ */
+require_once './vendor/autoload.php';
+
+use FormGuide\Handlx\FormHandler;
+
+
+$pp = new FormHandler(); 
+
+$validator = $pp->getValidator();
+$validator->fields(['name','email'])->areRequired()->maxLength(50);
+$validator->field('email')->isEmail();
+$validator->field('message')->maxLength(6000);
+
+
+$pp->attachFiles(['image']);
+
+
+$pp->sendEmailTo('wenbin.yan@gmail.com'); // ← Your email here
+
+echo $pp->process($_POST);
